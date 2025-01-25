cf0dcf2b12c4019b68663157de6cfce425c7056d
aovando <aovandodevsxr@gmail.com>
2025-01-24 21:07:41 -0500

    snap + move

---
+++ Assets/Scripts/AnimateHandController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Animator))]
+public class AnimateHandController : MonoBehaviour
+{
+    public InputActionReference gripInputActionReference;
+    public InputActionReference triggerInputActionReference;
+
+    private Animator handAnimator;
+    private float gripValue;
+    private float triggerValue;
+
+    private void Start()
+    {
+        handAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        AnimatorGrip();
+        AnimateTrigger();
+    }
+
+    private void AnimatorGrip()
+    {
+        gripValue = gripInputActionReference.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+    }
+
+    private void AnimateTrigger()
+    {
+        triggerValue = triggerInputActionReference.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+    }
+}
