7118b7d7b2d6b5971843fce6bb2086c8368d62fc
aovando <aovandodevsxr@gmail.com>
2025-01-25 13:55:39 -0500

    min/secs

--- Assets/Scripts/UI Handler/TimerCountdown.cs
+++ Assets/Scripts/UI Handler/TimerCountdown.cs
@@ -3,10 +3,30 @@ using UnityEngine;
 
 public class TimerCountdown : MonoBehaviour
 {
+
+    public float timerCountdown = 120f;
+
+    private void Update()
+    {
+        if (timerCountdown > 0)
+        {
+            timerCountdown -= Time.deltaTime;
+        }
+
+        else if (timerCountdown < 0)
+        {
+            timerText.text = "End Game";
+        }
+
+        int minutes = Mathf.FloorToInt(timerCountdown / 60);
+        int seconds = Mathf.FloorToInt(timerCountdown % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
     public TextMeshProUGUI timerText; 
     public void StartTimer()
     {
         gameObject.SetActive(true);
-        timerText.text = "Timer Start";
     }
 }
