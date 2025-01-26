bafa22bba722a49b7c0bb6dc585d6ac5a7f5c2b3
Andrew Bustos <infernious@live.com>
2025-01-26 13:58:05 -0800

    added rest of the audio

--- Assets/Scripts/Environment/AmberAlert.cs
+++ Assets/Scripts/Environment/AmberAlert.cs
@@ -11,6 +11,17 @@ public class AmberAlert : MonoBehaviour
         StartCoroutine(VibratePhone(6.0f)); 
         audioSource = GetComponent<AudioSource>();
         StartCoroutine(PlayNextAudio(4.5f));
+        StartCoroutine(PlayPlayerAudioIndex(2, 2.0f));
+        StartCoroutine(PlayPlayerAudioIndex(3, 10.0f));
+        StartCoroutine(PlayPlayerAudioIndex(4, 10.0f));
+        StartCoroutine(PlayPlayerAudioIndex(5, 10.0f));
+        StartCoroutine(PlayNPCAudioIndex(1, 10.0f));
+        StartCoroutine(PlayNPCAudioIndex(2, 10.0f));
+        StartCoroutine(PlayNPCAudioIndex(3, 10.0f));
+        StartCoroutine(PlayNPCAudioIndex(4, 10.0f));
+        StartCoroutine(PlayNPCAudioIndex(5, 10.0f));
+        StartCoroutine(PlayNPCAudioIndex(6, 10.0f));
+        StartCoroutine(PlayNPCAudioIndex(7, 10.0f));
     }
 
     IEnumerator VibratePhone(float delay)
@@ -28,5 +39,17 @@ public class AmberAlert : MonoBehaviour
         yield return new WaitForSeconds(delay);
         GameManager.Instance.PlayPlayerAudio(1);
         GameManager.Instance.PlayNPCAudio(0);
+    }
+
+    IEnumerator PlayPlayerAudioIndex(int index, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        GameManager.Instance.PlayPlayerAudio(index);
+    }
+
+    IEnumerator PlayNPCAudioIndex(int index, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        GameManager.Instance.PlayNPCAudio(index);
     }
 }
