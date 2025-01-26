582d7b813ae4e6f3c440f7f1ae79d9aec59577f8
aovando <aovandodevsxr@gmail.com>
2025-01-25 22:04:24 -0500

    partial interior

--- Assets/Scripts/Environment/SmallForce.cs
+++ Assets/Scripts/Environment/SmallForce.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class SmallForce : MonoBehaviour
 {
     private Rigidbody rb;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public void ObjectFall()
     {
