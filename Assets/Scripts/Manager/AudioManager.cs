302802069479a9180c26fbbe20fee7da8321da6c
aovando <aovandodevsxr@gmail.com>
2025-01-30 11:57:20 -0500

    audio cleanup

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -3,26 +3,6 @@ using System.Collections;
 using Unity.VisualScripting;
 using System;
 
-[System.Serializable]
-public class Sound
-{
-    public string name;
-    public AudioClip clip;
-    [Range(0, 1)]
-    public float volume = 1;
-    [Range(-3, 3)]
-    public float pitch = 1;
-    public bool loop = false;
-    public AudioSource source;
-
-    public Sound()
-    {
-        volume = 1;
-        pitch = 1;
-        loop = false;
-    }
-}
-
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
@@ -30,7 +10,6 @@ public class AudioManager : MonoBehaviour
     public AudioClip[] lastClipsToPlay;
     private AudioSource audioSource;
     private float audioLength;
-    public Sound[] sounds;
 
     void Awake()
     {
@@ -43,18 +22,6 @@ public class AudioManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
-
-        foreach (Sound s in sounds)
-        {
-            if (!s.source)
-                s.source = gameObject.AddComponent<AudioSource>();
-
-            s.source.clip = s.clip;
-
-            s.source.volume = s.volume;
-            s.source.pitch = s.pitch;
-            s.source.loop = s.loop;
-        }
     }
 
         private void Start()
@@ -115,24 +82,5 @@ public class AudioManager : MonoBehaviour
         }
 
         Debug.Log("Last Audio is Playing");
-    }
-
-    public void Play(string name)
-    {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s == null)
-        {
-            Debug.LogWarning("Sound: " + name + " not found");
-            return;
-        }
-
-        s.source.Play();
-    }
-
-    public void Stop(string name)
-    {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-
-        s.source.Stop();
     }
 }
